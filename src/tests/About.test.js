6a51db8492b8714c69c164870ce94a69e0d3ae8f
Gustavo Dimeira <gustavodimeira1@gmail.com>
2022-07-18 15:50:53 -0400

    Testes About

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,2 +1,14 @@
-test('', () => {});
-// mudanca para commit teste
+import React from 'react';
+import { screen, render } from '@testing-library/react';
+import About from '../pages/About';
+
+describe('Testando o compoente About', () => {
+  test('Testando o compoente About', () => {
+    render(<About />);
+    const title = screen.getByRole('heading', { name: /about pokédex/i });
+    const img = screen.getByRole('img', { name: /pokédex/i });
+    const srcExpectd = 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png';
+    expect(title).not.toBe(undefined);
+    expect(img.src).toBe(srcExpectd);
+  });
+});
