99c570181e6475ecec5f8cf0b8576d57f2007364
Gustavo Dimeira <gustavodimeira1@gmail.com>
2022-07-18 16:01:53 -0400

    Testes FavoritePokemon

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,11 @@
-test('', () => {});
+import React from 'react';
+import { screen, render } from '@testing-library/react';
+import FavoritePokemons from '../pages/FavoritePokemons';
+
+describe('Testando o componente Favorite Pokemon', () => {
+  test('Inicialmente a pagina Deve estar sem nenhum pokémon salvo', () => {
+    render(<FavoritePokemons />);
+    const titiulo = screen.getByText(/no favorite pokemon found/i);
+    expect(titiulo).toBeInTheDocument();
+  });
+});
