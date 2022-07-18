fa938839900384ff41c0a99e8bb7f4086e62a527
Gustavo Dimeira <gustavodimeira1@gmail.com>
2022-07-18 15:30:27 -0400

    Testes App

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,16 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import App from '../App';
+import renderWithRouter from '../helper/renderWithRouter';
+
+describe('Testando o App', () => {
+  test('ver se os links aparecem, com o nome correto', () => {
+    renderWithRouter(<App />);
+    const firstLink = screen.getByRole('link', { name: /home/i });
+    const secondLink = screen.getByRole('link', { name: /about/i });
+    const thirdLink = screen.getByRole('link', { name: /favorite pokémons/i });
+    expect(firstLink).not.toBe(undefined);
+    expect(secondLink).not.toBe(undefined);
+    expect(thirdLink).not.toBe(undefined);
+  });
+});
